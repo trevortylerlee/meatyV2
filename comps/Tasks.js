9b74377b2b8d0c0ef9a436cd8fd63bcc6f86eba5
Trevor Lee <tlee297@my.bcit.ca>
2022-05-01 00:32:13 -0700

    create taskbar component

---
+++ comps/Tasks.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 400px;
+  margin: 0 auto;
+  outline: 2px red solid;
+`
+
+export default function TaskBar() {
+  return <Container>
+    <h3>INSTRUCTIONS HERE</h3>
+  </Container>
+}
--- pages/chicken/index.js
+++ pages/chicken/index.js
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+
+import Nav from "../../comps/Nav"
+import TaskBar from "../../comps/Tasks"
+
+const Container = styled.div`
+  width: 400px;
+  margin: 0 auto;
+  outline: 2px red solid;
+`
+
 export default function ChickenIndex() {
   return <div>
-    <h1>Start of chicken</h1>
+    <Container>
+      <Nav />
+      <TaskBar />
+      <h1>Start of chicken</h1>
+    </Container>
   </div>
 }
