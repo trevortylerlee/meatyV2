34a58dedd97bd1d1c92492cccc9638507ee1c1b6
Trevor Lee <tlee297@my.bcit.ca>
2022-05-15 17:35:19 -0700

    add widget to index screen

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import useSound from 'use-sound';
+
 
 const Cont = styled.div`
 display:flex;
@@ -12,10 +14,9 @@ const ContentCont = styled.div`
 position:absolute;
 display:flex;
 flex-direction:column;
-align-items:center;
 justify-content:center;
-top:0;
-height:40vh;
+top:80px;
+height:0vh;
 width:100vw;
 `
 
@@ -24,20 +25,74 @@ height:100vh;
 width:100vw;
 object-fit:cover;
 `
-const Logo = styled.h1`
+const Logotype = styled.h1`
 position:relative;
-font: bold 64px Gill Sans, sans-serif;
+font: bold 48px Gill Sans, sans-serif;
+top: -16px;
+margin: 0;
 `
 const Start = styled.button`
 position:relative;
-margin-top:2rem;
-height:40px;
-width:80px;
-border:none;
-border-radius:4px;
-background:#871010;
-color:white;
-font:20px Ubuntu;
+background: white;
+margin: 0 auto;
+padding: 0.5rem 4rem;
+border-radius: 30px;
+border: 4px solid green;
+font-size: 1.5rem;
+margin-top: 20px;
+font-weight: bold;
+
+&:hover {
+    color: white;
+    background-color: green;
+    cursor: pointer;
+    text-decoration: underline;
+    transition: 0.3s;
+}
+&:active {
+    top: 2px;
+}
+`
+
+const Tile = styled.div`
+    position: relative;
+    text-align: center;
+    margin: 0 auto;
+    width: 380px;
+    background: rgba(250,250,255,0.9);
+    backdrop-filter: saturate(180%) blur(10px);
+    height: 60vh;
+    margin-top: 400px;
+    border-radius: 10px;
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    min-height: 360px;
+
+    .logoCont {
+        overflow: visible;
+        position: relative;
+        place-content: center;
+        height: 130px;
+        display: flex;
+    }
+    .logo {
+        background-image: url("/misc/logo.png");
+        background-size: 110px;
+        background-repeat: no-repeat;
+        width: 120px;
+        height: 120px;
+    }
+
+    p {
+        position: relative;
+        margin: 0;
+        margin-top: 54px;
+        font-size: 0.75rem;
+        color: lightgray;
+    }
+    p:hover {
+        color: black;
+    }
 `
 
 export default function(){
@@ -47,7 +102,23 @@ export default function(){
     if(r.pathname === "/"){
     return <Cont>
     <ContentCont>
-        <Logo>MEATY</Logo>
+            <Tile as={motion.div} className="tipCont" initial="onLoad" animate="visible" variants={{
+                onLoad: {
+                    opacity: 0
+                },
+                visible: {
+                    opacity: 1,
+                    scale: 1,
+                    transition: {
+                        duration: 0.8
+                    }
+                }
+            }}>
+        <div className="logoCont">
+        <div className="logo">
+        </div>
+        </div>
+        <Logotype>MEATY</Logotype>
         <Start onClick={
             ()=> {
                 {farm("/sounds/farm.mp3")};
@@ -55,6 +126,9 @@ export default function(){
                 pathname:"/disclaimer",
             })
         }}>Start</Start>
+        <p>Created by Trevor Lee, Ivan Li, and Ivan Tong</p>
+
+        </Tile>
     </ContentCont>
         <Splash src='/misc/splash.svg'></Splash>
     </Cont>
